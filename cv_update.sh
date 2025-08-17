0bfc7cd023b09d8ec551b5e8f70f6f41e431aa0d
Thiago Ferreira dos Santos <thiagoferreiradossantos@Thiagos-Laptop.wireless.yale.internal>
2025-08-17 10:21:19 -0400

    Update: moved images, deleted files, and other changes

--- cv_update.sh
+++ cv_update.sh
@@ -1,1 +1,1 @@
-mv /Users/thiagoferreiradossantos/Downloads/CV_thiagoferreira.pdf /Users/thiagoferreiradossantos/thiagofst.github.io
+mv /Users/thiagoferreiradossantos/Downloads/CV_thiagoferreira.pdf /Users/thiagoferreiradossantos/Downloads/thiagofst.github.io
--- update.sh
+++ update.sh
@@ -1,4 +1,9 @@
-git add index.html research.html publications.html media.html style.css
-git add POSTER*.png
+git add index.html research.html publications.html posters.html gallery.html style.css README.md
+
+#git add CV_thiagoferreira.pdf
+#git add POSTER*.png
+git add photos/*
+git add posters/*
+
 git commit -m "update"
 git push origin main
